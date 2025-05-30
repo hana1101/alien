5f61a1d3b6c41066117be3a1ce57aa8694ec8356
nahhnnn <now_id@snu.ac.kr>
2025-05-30 15:40:48 +0900

    zib2 update

--- startscene/zib2.js
+++ startscene/zib2.js
@@ -1,8 +1,8 @@
-let img;
+let img2;
 let box;
 
 function preload() {
-  img = loadImage("assets/zib2.png"); // 이미지 경로 확인
+  img2 = loadImage("assets/zib2.png");
 }
 
 function setup() {
@@ -16,41 +16,40 @@ function setup() {
 }
 
 function draw() {
-  image(img, 0, 0, width, height);
+  image(img2, 0, 0, width, height);
 
   // 대화 상자
   if (box) {
     box.display();
   }
 
-  // 오른쪽 상단 Dave 시점 태그
+  // 오른쪽 상단 zib 시점 태그
   let tagX = width - 200;
   let tagY = 20;
   let tagW = 180;
   let tagH = 60;
   let borderRadius = 30;
 
-  // 테두리
   fill(245, 31, 2);
   noStroke();
   rect(tagX, tagY, tagW, tagH, borderRadius);
 
-  // 내부 배경
   fill(201, 171, 167);
   rect(tagX + 5, tagY + 5, tagW - 10, tagH - 10, borderRadius - 5);
 
-  // 텍스트
   fill(0);
   textAlign(CENTER, CENTER);
   textSize(24);
   textStyle(BOLD);
-  text("Dave 시점", tagX + tagW / 2, tagY + tagH / 2);
+  text("ZIB 시점", tagX + tagW / 2, tagY + tagH / 2);
 }
 
-// 마우스 클릭으로 다음 대사
 function mousePressed() {
   if (box && !box.finished && box.isHovered()) {
     box.next();
+  } else if (box && box.finished) {
+    // 다음 scene3로 넘어갈 수 있게 준비
+    console.log("Go to scene3");
+    // 또는 scene = 3; loadScene(scene); 과 같이 연결
   }
 }
-
