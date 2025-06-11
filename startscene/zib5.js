23f2b32a3c31e8ee3f770cebb261258526565175
nahhnnn <now_id@snu.ac.kr>
2025-06-11 18:44:42 +0900

    .

---
+++ startscene/zib5.js
@@ -0,0 +1,14 @@
+let dialogueZib_scene5;
+
+function drawZib5() {
+  image(zib5, 0, 0, width, height);
+  if (!dialogueZib_scene5) {
+    let linesZib_scene5 = [
+    "DAVE: 나는 Dave,",
+    "DAVE: 출근도, 사랑도, 매일 똑같은 하루도 무의미해서 그냥 숨만 쉬고 있는 기분이다"];
+    dialogueZib_scene5 = new DialogueBox(10, 500, 980, 120, linesZib_scene5);
+  }
+
+  dialogueZib_scene5.display();
+
+}
