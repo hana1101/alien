e78619163db0cb3e8140d17f6c103471cb13b781
nahhnnn <now_id@snu.ac.kr>
2025-06-11 18:57:23 +0900

    .

---
+++ startscene/zib10.js
@@ -0,0 +1,13 @@
+let dialogueZib_scene10;
+
+function drawZib10() {
+  image(zib10, 0, 0, width, height);
+  if (!dialogueZib_scene10) {
+    let linesZib_scene10 = [
+    "ZIB: 이참에 남은 72시간을 조금 더 의미있게 보낼 수 있도록 무료한 이 친구의 삶에 작은 변화를 선물해줘야겠다."];
+    dialogueZib_scene10 = new DialogueBox(10, 500, 980, 120, linesZib_scene10);
+  }
+
+  dialogueZib_scene10.display();
+
+}
---
+++ startscene/zib8.js
@@ -0,0 +1,13 @@
+let dialogueZib_scene8;
+
+function drawZib8() {
+  image(zib8, 0, 0, width, height);
+  if (!dialogueZib_scene8) {
+    let linesZib_scene8 = [
+    "DAVE: 소리 나는 곳을 따라 가보니 외계인을 봤던 것 같은데, 정확한 기억은 나지 않는다.."];
+    dialogueZib_scene8 = new DialogueBox(10, 500, 980, 120, linesZib_scene8);
+  }
+
+  dialogueZib_scene8.display();
+
+}
