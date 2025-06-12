ef2103fbc4eb7d0dc05679494c835183aea25bcb
nahhnnn <now_id@snu.ac.kr>
2025-06-12 16:25:48 +0900

    .

--- scenes/wellbeingscreen.js
+++ scenes/wellbeingscreen.js
@@ -1,27 +1,30 @@
-//function drawWellbeingcreen() {
-//   if (!dialogue) {
-//     let lines = [
-//       "ZIB: 일기장",
-//       "ZIB: 내가 지구에서 있는동안 할 수 있는 경험은 다 해보고 가야지",
-//       "ZIB: 그럼 이 친구 직업이..",
-//     ];
-//     dialogue = new DialogueBox(10, 500, 980, 120, lines);
-//   }
+let glowAlpha = 0;
+let glowDirection = 2;
 
-//   image(assets.workplace, 0, 0, width, height);
-//   dialogue.display();
+function drawWellbeingscreen() {
+if (!dialogue) {
+    let lines = [
+      "ZIB: 저런,, 요즘 많이 힘들구나",
+      "ZIB: 버디랑 시간을 보내봐야지",
+    ];
+    dialogue = new DialogueBox(10, 500, 980, 120, lines);
+  }
+
+  image(assets.diary, 0, 0, width, height);
+  dialogue.display();
   
-// if (dialogue && dialogue.isOnLastLine()) {
-//   let alpha = 127 + 127 * sin(millis() / 300);
+if (dialogue && dialogue.isOnLastLine()) {
+  let alpha = 127 + 127 * sin(millis() / 300);
 
-//   textAlign(RIGHT, BOTTOM);
-//   textFont("Press Start 2P");
-//   textSize(14);
-//   fill(0, 255, 153, alpha);
-//   let padding = 20;
-//   text(
-//     "CLICK HERE TO CONTINUE",
-//     dialogue.x + dialogue.w - padding,
-//     dialogue.y + dialogue.h - padding
-//   );
-// }
+  textAlign(RIGHT, BOTTOM);
+  textFont("Press Start 2P");
+  textSize(14);
+  fill(0, 255, 153, alpha);
+  let padding = 20;
+  text(
+    "CLICK HERE TO CONTINUE",
+    dialogue.x + dialogue.w - padding,
+    dialogue.y + dialogue.h - padding
+  );
+}
+}
