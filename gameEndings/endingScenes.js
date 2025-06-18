353baaec7c15df784cf52164d612e6f8406f7828
nahhnnn <now_id@snu.ac.kr>
2025-06-19 08:14:51 +0900

    .

--- gameEndings/endingScenes.js
+++ gameEndings/endingScenes.js
@@ -9,15 +9,24 @@ function calculatePointsPage(){
     displayStats();
 }
 
+let dialogueEnd_scene1;
 function drawHappyEnding1(){
     image(happyEnding1,0,0,width,height);
+    if (!dialogueEnd_scene1) {
+    let linesEnd_scene1 = [
+    "DAVE에서 다시 ZIB의 몸으로 돌아가는 중.."];
+    dialogueEnd_scene1 = new DialogueBox(10, 500, 980, 120, linesEnd_scene1);
+  }
+  dialogueEnd_scene1.display();
 }
+
 function drawHappyEnding2(){
     image(happyEnding2,0,0,width,height);
 }
 function drawHappyEnding3(){
     image(happyEnding3,0,0,width,height);
 }
+
 
 function drawBadEnding1(){
     image(badEnding1,0,0,width,height);
--- girlfriendFT/endingFT.js
+++ girlfriendFT/endingFT.js
@@ -1,10 +1,11 @@
 let nextBtnEndFT
 
 function endingSceneFT(){
-    background(225);
-    push();
-    text(CENTER);
-    textAlign("Cant play again");
+    image(unav, 0, 0, width, height);
+    // background(225);
+    // push();
+    // text(CENTER);
+    // textAlign("Cant play again");
     nextBtnEndFT = new Button(width-150, height / 2 + 50+195, 130,55, "Next", nextGame);
     nextBtnEndFT.display();
 
